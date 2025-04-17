cc899ae28c11944863de8bde10de08273a8f677e
Victor <victorfreire78965@gmail.com>
2025-04-16 22:13:33 -0300

    desenvolvendo o backend

---
+++ index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+	int soma,num1,num2;
+
+	printf("Informe o primeiro numero para a soma:");
+	scanf("%d", &num1);
+
+	printf("Informe o segundo número:");
+	scanf("%d", &num2);
+
+	soma = num1 + num2;
+	printf("%d + %d = %d", num1, num2, soma);
+
+	return 0;
+}
